cb538de82e8d8ff2089e80bf8d46cda38db8628c
pawan-nirpal-031 <pawannirpal@gmail.com>
2022-05-20 23:12:57 +0530

    revise

---
+++ Assembly/Tutorial/hello.asm
@@ -0,0 +1,20 @@
+section .data 
+	msg db 'Hello, World',10
+	len equ $-msg 
+
+section .text
+	global _start
+
+_start:
+	mov rax,1 
+	mov rdi,1 
+	mov rsi, msg 
+	mov rdx,len 
+	syscall
+
+	mov rax,60 
+	xor rdi,rdi 
+	syscall 
+
+	
+	
